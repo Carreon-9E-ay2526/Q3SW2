e7b5051e7637293a59c48cb07efd73b3609a7e33
Carreon-9E-ay2526 <carreon.jo@obmontessori.edu.ph>
2026-02-27 09:55:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function getDiscount(){
+    let budg = document.getElementById("cost").value;
+    let disva = Number(budg * 0.2);
+
+    document.getElementById("disdisbut").innerHTML = `
+        <div class="container bg-secondary">
+            <h2 class="text-white">Recommended Brands:</h2>
+            <button class="btn btn-success"> Acer </button>
+            <button class="btn btn-success"> Asus </button>
+            <button class="btn btn-success"> Dell </button>
+            <button class="btn btn-success"> Lenovo </button>
+            <button class="btn btn-success"> MSI  </button>
+            <p>Eligible Discount:<span class="bg-danger">${disva}</span></p>
+        </div>
+    `
+}
+
+function getLoction(){
+    document.getElementById("Storeloc").innerHTML = `
+        <div class="container bg-secondary">
+                    <img src="PC Express LOGO.webp" width="200px">
+                    <hr>
+                    <p>
+                        Quezon City: ROG Concept Store SM North<br>
+                        Mandaluyong City: PC Express - SM Megamall<br>
+                        Caloocan City: SM Caloocan! We're excited to be located CZ-13 3F SM<br>
+                        Manila City: SM San Lazaro! We're excited to be located on the 3/F CYBERZONE SM SAN LAZARO, FELIX HUERTAS STREET CORNER LACSON AVENUE (C-2 ROAD), SANTA CRUZ<br>
+                    </p>
+        </div>
+    `
+}
